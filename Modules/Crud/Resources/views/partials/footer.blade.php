f472363c624b83544deec94a7dd79086627e6314
Sarfraz <sarfraz@eteamid.com>
2018-01-23 17:01:47 +0500

    updated

--- Modules/Crud/Resources/views/partials/footer.blade.php
+++ Modules/Crud/Resources/views/partials/footer.blade.php
@@ -1,11 +1,12 @@
 <div class="clearfix">&nbsp;</div>
 
 <!-- Scripts -->
-<script href="/modules/core/js/jquery.js"></script>
-<script href="/modules/core/css/bootstrap/js/bootstrap.min.js"></script>
-<script href="/modules/core/js/plugins/select2/select2.full.min.js"></script>
-<script href="/modules/core/js/plugins/sweetalert/dist/sweetalert.min.js"></script>
-<script href="/modules/crud/js/custom.js"></script>
+<script src="/modules/core/js/jquery.js"></script>
+<script src="/modules/core/css/bootstrap/popper.min.js"></script>
+<script src="/modules/core/css/bootstrap/bootstrap.min.js"></script>
+<script src="/modules/core/js/plugins/select2/select2.full.min.js"></script>
+<script src="/modules/core/js/plugins/sweetalert/dist/sweetalert.min.js"></script>
+<script src="/modules/crud/js/custom.js"></script>
 
 @stack('scripts')
 
