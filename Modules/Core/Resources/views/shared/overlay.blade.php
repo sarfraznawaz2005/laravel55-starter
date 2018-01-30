dd200b3fcaa5aa45e4f3da7ab26cfa5251979ded
Sarfraz <sarfraz@eteamid.com>
2018-01-30 19:16:54 +0500

    updated

--- Modules/Core/Resources/views/shared/overlay.blade.php
+++ Modules/Core/Resources/views/shared/overlay.blade.php
@@ -1,23 +1,37 @@
-<style>
-    #__cover__ {
-        position: fixed;
-        top: 0;
-        left: 0;
-        background: rgba(0, 0, 0, 0.6);
-        color: #fff;
-        font-size: 20px;
-        text-align: center;
-        z-index: 5;
-        width: 100%;
-        height: 100%;
-        display: none;
-    }
+@push('styles')
+    <style>
+        #__cover__ {
+            position: fixed;
+            top: 0;
+            left: 0;
+            background: rgba(0, 0, 0, 0.6);
+            z-index: 999999999999999;
+            width: 100%;
+            height: 100%;
+            display: none;
+            justify-content: center;
+            flex-direction: column;
+            text-align: center;
+            font-size: 175%;
+            font-weight:bold;
+            color:#fff;
+        }
+    </style>
+@endpush
 
-    #__cover__ span {
-        display: table-cell;
-        vertical-align: middle;
-    }
-</style>
+@push('scripts')
+    <script>
+        function showOverlay(message) {
+            var message = message || 'Please wait...';
+
+            $('#__cover__').html(message).css('display', 'flex');
+        }
+
+        function hideOverlay() {
+            $('#__cover__').css('display', 'none');
+        }
+    </script>
+@endpush
 
 <div id="__cover__" align="center">
     <span>Please wait...</span>
