970afed68a3480543a5f8524122d19bf40c0c42d
Sarfraz <sarfraz@eteamid.com>
2018-02-01 15:23:52 +0500

    updated

--- Modules/Main/Resources/views/layouts/partials/head.blade.php
+++ Modules/Main/Resources/views/layouts/partials/head.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, shrink-to-fit=no">
-    <meta name="author" content="Sarfraz Ahmed">
+    <meta name="author" content="Sarfraz Ahmed (sarfraznawaz2005@gmail.com)">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{Meta::get('title') . ' :: ' . appName()}}</title>
 
