877f77ecfcdd1d94b1c1c9cf54be19c0281a35a9
Sarfraz <sarfraznawaz2005@gmail.com>
2018-02-14 22:43:15 +0500

    updated

--- Modules/Admin/Resources/views/pages/panel/index.blade.php
+++ Modules/Admin/Resources/views/pages/panel/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
 
         <div class="col-md-3">
-            <a href="{{active('admin_user_listing')}}" class="widget-link">
+            <a href="{{route('admin_user_listing')}}" class="widget-link">
                 <div class="widget-small primary"><i class="icon fa fa-users fa-3x"></i>
                     <div class="info">
                         <h4>Users</h4>
