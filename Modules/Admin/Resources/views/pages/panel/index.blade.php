9e949c3e284574e62c8f46952130fea16caef492
Sarfraz <sarfraznawaz2005@gmail.com>
2018-02-14 21:09:13 +0500

    updated

--- Modules/Admin/Resources/views/pages/panel/index.blade.php
+++ Modules/Admin/Resources/views/pages/panel/index.blade.php
@@ -3,37 +3,47 @@
 @section('content')
 
     <div class="row">
+
         <div class="col-md-3">
-            <div class="widget-small primary"><i class="icon fa fa-users fa-3x"></i>
-                <div class="info">
-                    <h4>Users</h4>
-                    <p><b>5</b></p>
+            <a href="{{active('admin_user_listing')}}" class="widget-link">
+                <div class="widget-small primary"><i class="icon fa fa-users fa-3x"></i>
+                    <div class="info">
+                        <h4>Users</h4>
+                        <p><b>5</b></p>
+                    </div>
                 </div>
-            </div>
+            </a>
+        </div>
+
+        <div class="col-md-3">
+            <a href="#" class="widget-link">
+                <div class="widget-small success"><i class="icon fa fa-thumbs-o-up fa-3x"></i>
+                    <div class="info">
+                        <h4>Likes</h4>
+                        <p><b>25</b></p>
+                    </div>
+                </div>
+            </a>
         </div>
         <div class="col-md-3">
-            <div class="widget-small success"><i class="icon fa fa-thumbs-o-up fa-3x"></i>
-                <div class="info">
-                    <h4>Likes</h4>
-                    <p><b>25</b></p>
+            <a href="#" class="widget-link">
+                <div class="widget-small warning"><i class="icon fa fa-files-o fa-3x"></i>
+                    <div class="info">
+                        <h4>Uploades</h4>
+                        <p><b>10</b></p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
         <div class="col-md-3">
-            <div class="widget-small warning"><i class="icon fa fa-files-o fa-3x"></i>
-                <div class="info">
-                    <h4>Uploades</h4>
-                    <p><b>10</b></p>
+            <a href="#" class="widget-link">
+                <div class="widget-small danger"><i class="icon fa fa-star fa-3x"></i>
+                    <div class="info">
+                        <h4>Stars</h4>
+                        <p><b>500</b></p>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="widget-small danger"><i class="icon fa fa-star fa-3x"></i>
-                <div class="info">
-                    <h4>Stars</h4>
-                    <p><b>500</b></p>
-                </div>
-            </div>
+            </a>
         </div>
     </div>
 
@@ -93,13 +103,15 @@
                         <p><b>Fliping Toggle Button</b></p>
                         <div class="toggle-flip">
                             <label>
-                                <input type="checkbox"><span class="flip-indecator" data-toggle-on="ON" data-toggle-off="OFF"></span>
+                                <input type="checkbox"><span class="flip-indecator" data-toggle-on="ON"
+                                                             data-toggle-off="OFF"></span>
                             </label>
                         </div>
                         <h5>Disabled state</h5>
                         <div class="toggle-flip">
                             <label>
-                                <input type="checkbox" disabled=""><span class="flip-indecator" data-toggle-on="ON" data-toggle-off="OFF"></span>
+                                <input type="checkbox" disabled=""><span class="flip-indecator" data-toggle-on="ON"
+                                                                         data-toggle-off="OFF"></span>
                             </label>
                         </div>
                     </div>
