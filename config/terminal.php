dd7099ac74d41323ada9e0158b5dc9f8cc1d8aab
Sarfraz Ahmed <sarfraz@onsupport.com>
2018-08-02 12:18:22 +0500

    updated

--- config/terminal.php
+++ config/terminal.php
@@ -29,7 +29,7 @@ return [
     'commands' => [
         \Recca0120\Terminal\Console\Commands\Artisan::class,
         \Recca0120\Terminal\Console\Commands\ArtisanTinker::class,
-        \Recca0120\Terminal\Console\Commands\Cleanup::class,
+        //\Recca0120\Terminal\Console\Commands\Cleanup::class,
         \Recca0120\Terminal\Console\Commands\Composer::class,
         \Recca0120\Terminal\Console\Commands\Find::class,
         \Recca0120\Terminal\Console\Commands\Mysql::class,
