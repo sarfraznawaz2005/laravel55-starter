aa21860e79ea529c2fbf0446bd704b5ee9a4ef70
Sarfraz Ahmed <sarfraz@onsupport.com>
2018-10-02 18:28:16 +0500

    updated

--- Modules/Main/Resources/views/layouts/partials/head.blade.php
+++ Modules/Main/Resources/views/layouts/partials/head.blade.php
@@ -3,6 +3,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, shrink-to-fit=no">
     <meta name="author" content="Sarfraz Ahmed (sarfraznawaz2005@gmail.com)">
+
+    @if(request()->isSecure())
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+    @endif
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{Meta::get('title') . ' :: ' . appName()}}</title>
 
